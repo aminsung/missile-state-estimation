09e24cdefc387feb3cad949ce8f1ea451bc1f234
Min Sung Ahn <aminsung@gmail.com>
2016-03-13 04:52:09 -0700

    Got basic global constants and state space done

--- main.py
+++ main.py
@@ -17,4 +17,24 @@ W = 100.0e2 # [(ft/sec^2)^2]
 DT = 0.01 # [sec]
 T = np.arange(0.0, 10.0, DT) # [sec]
 
+# Dynamic State
+F = np.array([[0.0, 1.0, 0.0], [0.0, 0.0 -1.0], [0.0, 0.0, -(1/TAU)]])
+G = np.array([[0], [0], [1]])
+
+# ============================================================================
+# Kalman Filter
+# Status: WIP
+# ============================================================================
+P_init = np.array([[0.0, 0.0, 0.0], [0.0, 200.0e2, 0.0], [0.0, 0.0, 100.0e2]])
+P_history = np.zeros((3, 3, T.size))
+K_history = np.zeros((3, 3, T.size))
+Z_history = np.zeros(T.size)
+x_hat_history = np.zeros((3, 1, T.size))
+dx_hat_history = np.zeros((3, 1, T.size))
+x_history = np.zeros((3, 1, T.size))
+dx_history = np.zeros((3, 1, T.size))
+err_history = np.zeros((3, 1, T.size))
+residual = np.zeros((2, 1))
+
+
 print("Missile State Estimation Done")
